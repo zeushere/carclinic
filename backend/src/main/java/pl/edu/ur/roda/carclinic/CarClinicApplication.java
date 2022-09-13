6d280c5bcb25bb2fdefa83ffb047b317ac891084
DESKTOP-FEFTN30\zeushere <kacper.roda37@gmail.com>
2022-09-13 15:36:43 +0200

    Initial commit

---
+++ backend/src/main/java/pl/edu/ur/roda/carclinic/CarClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.ur.roda.carclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarClinicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/edu/ur/roda/carclinic/CarClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ur.roda.carclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
