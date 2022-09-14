d91c90960d21255e64db851180d58558931ec07e
zeushere <kacper.roda37@gmail.com>
2022-09-14 10:34:31 +0200

    Added controller handler

---
+++ backend/src/main/java/pl/edu/ur/roda/carclinic/util/ControllerHandler.java
@@ -0,0 +1,28 @@
+package pl.edu.ur.roda.carclinic.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = exception.getBindingResult().getAllErrors().stream()
+                .map(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    return new AbstractMap.SimpleEntry<>(fieldName, errorMessage);
+                }).collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+        return new ResponseEntity(errors, HttpStatus.CONFLICT);
+    }
+
+}
