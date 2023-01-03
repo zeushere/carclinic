bec9eefba202ee0bf5407b791394b9e5e54957f6
zeushere <kacper.roda37@gmail.com>
2023-01-03 23:33:47 +0100

    Work in Progress to pass form-data for car add

--- frontend/src/components/UI/AddCarForm.jsx
+++ frontend/src/components/UI/AddCarForm.jsx
@@ -1,36 +1,94 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "../../styles/find-car-form.css";
 import {Col, Form, FormGroup, Row} from "reactstrap";
 import '../../styles/add-car-form.css';
 import useWindowDimensions from "../WindowDimension/WindowDimension";
+import {useDispatch, useSelector} from "react-redux";
+import Axios from "axios";
+import {json} from "react-router-dom";
 
 const AddCarForm = () => {
     const {width } = useWindowDimensions();
+    const formData = new FormData();
+    const dispatch = useDispatch();
+    const [brand, setBrand] = useState('');
+    const [model, setModel] = useState('');
+    const [yearProduction, setYearProduction] = useState('');
+    const [engineType, setEngineType] = useState('');
+    const [engineCapacity, setEngineCapacity] = useState('');
+    const [description, setDescription] = useState('');
+    const [carAdded, setCarAdded] = useState(false);
+    const userSignin = useSelector(state => state.userSignin);
+    const { userInfo } = userSignin;
+
+    const carRequest = {
+        brand,
+        model,
+        yearProduction,
+        engineType,
+        engineCapacity,
+        description
+    }
+
+    const json = JSON.stringify(carRequest);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    formData.append("carRequest" , JSON.parse(json));
+    dispatch(addCar(formData))
+    }
+
+    const addCar = (formData) => async (e) => {
+        try {
+            const {data} = await Axios.post('/cars', formData, {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            });
+            setCarAdded(true);
+        }
+        catch (error) {
+            setCarAdded(false);
+        }
+    };
     return (
-        <Form className="form">
+        <Form className="form" onSubmit={handleSubmit}>
             <div className=" d-flex justify-content-around flex-wrap">
                 <FormGroup className="form__group">
-                    <input type="text" placeholder="Marka" />
+                    <input type="text"
+                           placeholder="Marka"
+                           value={brand}
+                           onChange={(e) => setBrand(e.target.value)} />
                 </FormGroup>
 
                 <FormGroup className="form__group">
-                    <input type="text" placeholder="Model" required />
+                    <input type="text" placeholder="Model" required
+                           value={model}
+                           onChange={(e) => setModel(e.target.value)} />
                 </FormGroup>
 
                 <FormGroup className="form__group">
-                    <input type="text" placeholder="Rok produkcji" />
+                    <input type="text" placeholder="Rok produkcji"
+                           value={yearProduction}
+                           onChange={(e) => setYearProduction(e.target.value)} />
                 </FormGroup>
 
                 <FormGroup className="form__group">
-                    <input type="text" placeholder="Typ silnika"/>
+                    <input type="text" placeholder="Typ silnika"
+                           value={engineType}
+                           onChange={(e) => setEngineType(e.target.value)} />
                 </FormGroup>
                 <FormGroup className="justify-content-b form__group">
-                    <input type="text" placeholder="Pojemność silnika"/>
+                    <input type="text" placeholder="Pojemność silnika"
+                           value={engineCapacity}
+                           onChange={(e) => setEngineCapacity(e.target.value)} />
                 </FormGroup>
             </div>
             <div className={'d-flex flex-wrap justify-content-around mt-2'}>
-                <textarea type="text" className='textarea__add__car__form mb-0' placeholder="Opis samochodu"/>
-                <button className={width >= 768 ? "btn add__car-btn" : "btn add__car-btn mt-2"}>Dodaj samochód</button>
+                <textarea type="text" className='textarea__add__car__form mb-0' placeholder="Opis samochodu"
+                          value={description}
+                          onChange={(e) => setDescription(e.target.value)} />
+                <button type={"submit"} className={width >= 768 ? "btn add__car-btn" : "btn add__car-btn mt-2"}>Dodaj samochód</button>
 
             </div>
 
--- frontend/src/pages/ClientCars.jsx
+++ frontend/src/pages/ClientCars.jsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import ClientCar from "./ClientCar";
 import {Col, Container, Row} from "reactstrap";
-import carData from "../assets/data/carData";
-import CarItem from "../components/UI/CarItem";
 import {useDispatch, useSelector} from "react-redux";
 import {listCars} from "../actions/carActions";
 import SnackbarType from "../components/Snackbar/SnackbarType";
@@ -11,31 +9,31 @@ import {CAR_DELETE_RESET} from "../constants/carConstants";
 import useWindowDimensions from "../components/WindowDimension/WindowDimension";
 import {Link} from "react-router-dom";
 import '../styles/client-cars.css'
-import FindCarForm from "../components/UI/FindCarForm";
 import AddCarForm from "../components/UI/AddCarForm";
 
 const ClientCars = (props) => {
     const dispatch = useDispatch();
     const carList = useSelector(state => state.carList);
     const snackbarRef = useRef(null);
-    const { loading, error, cars } = carList;
+    const {loading, error, cars} = carList;
     const carDelete = useSelector((state) => state.carDelete);
-    const { loading: loadingDelete, error: errorDelete, success: successDelete } = carDelete;
-    const {width } = useWindowDimensions();
+    const {loading: loadingDelete, error: errorDelete, success: successDelete} = carDelete;
+    const {width} = useWindowDimensions();
     const [addCarViewFlag, setAddCarViewFlag] = useState(false);
 
-   function addCar() {
-        return <div className="car__form" >
+    function addCar() {
+        return <div className="car__form">
             <Container>
                 <Row className="form__row">
                     <Col lg="5" md="12">
                         <div className="find__cars-left">
-                            <h2 className={'text-center'}>Wstaw zdjęcie</h2>
+                            <h2 className={'text-center'}><Link className={'insert__photo__button'} to="#">Wstaw
+                                zdjęcie</Link></h2>
                         </div>
                     </Col>
 
                     <Col lg="7" md="12" sm="12">
-                        <AddCarForm />
+                        <AddCarForm/>
                     </Col>
                 </Row>
             </Container>
@@ -44,38 +42,39 @@ const ClientCars = (props) => {
 
     useEffect(() => {
         dispatch(listCars());
-        if(successDelete === true){
+        if (successDelete === true) {
             snackbarRef.current.show();
-                dispatch({ type: CAR_DELETE_RESET });
+            dispatch({type: CAR_DELETE_RESET});
         }
-        }, [dispatch, successDelete]);
+    }, [dispatch, successDelete]);
 
-    return(
-          <section>
-              <Container>
-                  <Row>
-                      <Col lg="7" md='12' className={width >= 992 ? 'text-end mb-5' : 'text-center mb-5'}>
-                          <h2 className="section__title">Twoje samochody</h2>
-                      </Col>
-                      <Col lg="5" md='12' className={width >= 992 ? 'text-end mb-5' : 'text-center mb-5'}>
-                          <button className="header__btn btn car__add__button" onClick={() => setAddCarViewFlag(!addCarViewFlag)}>
-                              <Link to="#">
-                                  <i className="ri-car-line"></i> Dodaj samochód
-                              </Link>
-                          </button>
-                      </Col>
-                      {addCarViewFlag ? addCar() : null}
-                      {cars?.map((car) => (
-                          <ClientCar car={car} key={car.id} />
-                      ))}
-                      </Row>
-                  <Snackbar
-                      ref={snackbarRef}
-                      message="Pomyślnie usunięto samochód!"
-                      type={SnackbarType.success}
-                  />
-              </Container>
-          </section>
+    return (
+        <section>
+            <Container>
+                <Row>
+                    <Col lg="7" md='12' className={width >= 992 ? 'text-end mb-5' : 'text-center mb-5'}>
+                        <h2 className="section__title">Twoje samochody</h2>
+                    </Col>
+                    <Col lg="5" md='12' className={width >= 992 ? 'text-end mb-5' : 'text-center mb-5'}>
+                        <button className="header__btn btn car__add__button"
+                                onClick={() => setAddCarViewFlag(!addCarViewFlag)}>
+                            <Link to="#">
+                                <i className="ri-car-line"></i> Dodaj samochód
+                            </Link>
+                        </button>
+                    </Col>
+                    {addCarViewFlag ? addCar() : null}
+                    {cars?.map((car) => (
+                        <ClientCar car={car} key={car.id}/>
+                    ))}
+                </Row>
+                <Snackbar
+                    ref={snackbarRef}
+                    message="Pomyślnie usunięto samochód!"
+                    type={SnackbarType.success}
+                />
+            </Container>
+        </section>
     );
 }
 
